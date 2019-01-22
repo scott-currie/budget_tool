f9727d14f1d02829f031fc68e6d00b418332d17d
Scott Currie <vitriolik@gmail.com>
2019-01-22 15:18:25 -0800

    Built out class based views.

--- budgets/views.py
+++ budgets/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
+from .models import Budget, Transaction
 
-class BudgetListView(ListView):
+
+class BudgetListView(LoginRequiredMixin, ListView):
     template_name = 'budget/budget_list.html'
     context_object_name = 'budgets'
     login_url = reverse_lazy('login')
 
     def get_queryset(self):
-        return
+        return Budget.objects.filter(user__username=self.request.user.username)
 
-class BudgetDetailView(ListView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['budgets'] = Budget.objects.filter(user__username=self.request.user.username)
+        return context
+
+
+class BudgetDetailView(LoginRequiredMixin, DetailView):
     template_name = 'budget/budget_detail.html'
+    model = Transaction
+    context_object_name = 'transaction'
+    login_url = reverse_lazy('login')
+    pk_url_kwarg = 'id'
+
+    def get_queryset(self):
+        return Transaction.objects.filter(budget__user__username=self.request.user.username)

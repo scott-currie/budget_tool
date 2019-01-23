0dac8848171763553023ccb1a4d4e4ab07c7c860
Scott Currie <vitriolik@gmail.com>
2019-01-23 08:42:03 -0800

    Added create views.

--- budgets/views.py
+++ budgets/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Budget, Transaction
+from .forms import BudgetForm, TransactionForm
 
 
 class BudgetListView(LoginRequiredMixin, ListView):
@@ -33,3 +34,29 @@ class BudgetDetailView(LoginRequiredMixin, DetailView):
         context = super().get_context_data(**kwargs)
         context['transactions'] = Transaction.objects.filter(budget__id=self.kwargs[self.pk_url_kwarg])
         return context
+
+
+class BudgetCreateView(LoginRequiredMixin, CreateView):
+    template_name = 'budget/budget_create.html'
+    model = Budget
+    form_class = BudgetForm
+    success_url = reverse_lazy('budget_list_view')
+    login_url = reverse_lazy('auth_login')
+
+    def form_valid(self, form):
+        """Validate form data."""
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class TransactionCreateView(LoginRequiredMixin, CreateView):
+    template_name = 'budget/transaction_create.html'
+    model = Transaction
+    form_class = TransactionForm
+    success_url = reverse_lazy('budget_list_view')
+    login_url = reverse_lazy('auth_login')
+
+    def form_valid(self, form):
+        """Validate form data."""
+        form.instance.user = self.request.user
+        return super().form_valid(form)

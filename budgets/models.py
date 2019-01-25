27d8ec3bbfecdd531528081d04099354f8c84257
Scott Currie <vitriolik@gmail.com>
2019-01-25 08:11:24 -0800

    Code formatting.

--- budgets/models.py
+++ budgets/models.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class Budget(models.Model):
     """Class representing Budget objects."""
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='budgets')
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='budgets')
     name = models.CharField(max_length=1024)
     total_budget = models.FloatField()
     remaining_budget = models.FloatField()
@@ -11,7 +15,10 @@ class Budget(models.Model):
 
 class Transaction(models.Model):
     """Class representing Transaction objects"""
-    budget = models.ForeignKey(Budget, on_delete=models.CASCADE, related_name='transactions')
+    budget = models.ForeignKey(
+        Budget,
+        on_delete=models.CASCADE,
+        related_name='transactions')
     TRANS_TYPES = [('withdrawal', 'Withdrawal'),
                    ('deposit', 'Deposit')]
     type = models.CharField(max_length=1024, choices=TRANS_TYPES)
